6a2ccd89428071fa5e28fd81aaaf11c884171d82
CPPGM Support <support@cppgm.org>
2013-03-12 01:51:58 +0100

    Missing include from stub

--- ctrlexpr.cpp
+++ ctrlexpr.cpp
@@ -1,5 +1,9 @@
 // (C) 2013 CPPGM Foundation www.cppgm.org.  All rights reserved.
 
+#include <string>
+
+using namespace std;
+
 // mock implementation of IsDefinedIdentifier for PA3
 // return true iff first code point is odd
 bool PA3Mock_IsDefinedIdentifier(const string& identifier)
